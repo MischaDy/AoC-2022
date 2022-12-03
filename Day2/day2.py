e61a7c73edef0a461b31581a69932ebc9f7869e6
Mischa <MischaDy@users.noreply.github.com>
2022-12-03 11:18:42 +0100

    Day2: Slightly improved solution (refactoring)

--- Day2/day2.py
+++ Day2/day2.py
@@ -4,11 +4,6 @@ PART = 2
 TEST_INPUT_PATH = 'test_input.txt'
 INPUT_PATH = 'input.txt'
 
-SHAPE_POINTS_DICT1 = dict(zip("XYZ", [1, 2, 3]))
-SHAPE_POINTS_DICT2 = dict(zip("ABC", [1, 2, 3]))
-OUTCOME_POINTS_DICT1 = dict(zip([0, 1, 2], [6, 3, 0]))  # order: I win, I draw, I lose
-OUTCOME_POINTS_DICT2 = dict(zip("XYZ", [0, 3, 6]))  # order: I lose, I draw, I win
-
 
 def main(run_test, part, test_input_path, input_path):
     file_path = test_input_path if run_test else input_path
@@ -23,13 +18,15 @@ def day2_part1(input_):
 
 
 def calc_points_part1(round_):
+    shape_points_dict = dict(zip("XYZ", [1, 2, 3]))
+    outcome_points_dict = dict(zip([0, 1, 2], [6, 3, 0]))  # order: I win, I draw, I lose
     outcome_dict = {"X": "CAB", "Y": "ABC", "Z": "BCA"}  # order: I win, I draw, I lose
 
     shape_opp, shape_me = round_.split(' ')
-    shape_points = SHAPE_POINTS_DICT1[shape_me]
+    shape_points = shape_points_dict[shape_me]
 
     outcome = outcome_dict[shape_me].index(shape_opp)
-    outcome_points = OUTCOME_POINTS_DICT1[outcome]
+    outcome_points = outcome_points_dict[outcome]
     return shape_points + outcome_points
 
 
@@ -39,14 +36,17 @@ def day2_part2(input_):
 
 
 def calc_points_part2(round_):
+    shape_points_dict = dict(zip("ABC", [1, 2, 3]))
+    outcome_points_dict = dict(zip("XYZ", [0, 3, 6]))  # order: I lose, I draw, I win
+
     shape_me_dict = {"A": dict(zip("XYZ", "CAB")),
                      "B": dict(zip("XYZ", "ABC")),
                      "C": dict(zip("XYZ", "BCA"))}
 
     shape_opp, outcome_needed = round_.split(' ')
-    outcome_points = OUTCOME_POINTS_DICT2[outcome_needed]
+    outcome_points = outcome_points_dict[outcome_needed]
     shape_me = shape_me_dict[shape_opp][outcome_needed]
-    shape_points = SHAPE_POINTS_DICT2[shape_me]
+    shape_points = shape_points_dict[shape_me]
     return outcome_points + shape_points
 
 

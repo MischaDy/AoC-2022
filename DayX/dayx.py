3ebdff60ffb51060f4af47236f74f41ef3bc8efa
Mischa <MischaDy@users.noreply.github.com>
2022-12-05 14:32:12 +0100

    Improved base files

--- DayX/dayx.py
+++ DayX/dayx.py
@@ -7,23 +7,25 @@ INPUT_PATH = 'input.txt'
 
 def main(run_test, part, test_input_path, input_path):
     file_path = test_input_path if run_test else input_path
-    day_function = dayX_part1 if part == 1 else dayX_part2
-    input_ = get_input(file_path)
+    day_function = dayx_part1 if part == 1 else dayx_part2
+    input_ = get_input(file_path, line_sep='\n')
     print(day_function(input_))
 
 
-def dayX_part1(input_):
+def dayx_part1(input_):
     pass
 
 
-def dayX_part2(input_):
+def dayx_part2(input_):
     pass
 
 
-def get_input(file_path):
+def get_input(file_path, line_sep='\n'):
     with open(file_path) as f:
-        input_ = ...
+        input_ = f.read()
 
+    if line_sep:
+        input_ = input_.split(line_sep)
     return input_
 
 

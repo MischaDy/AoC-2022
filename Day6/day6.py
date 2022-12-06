ab141a11b57e289e1c4ee36214df0cc767ce5f6e
Mischa <MischaDy@users.noreply.github.com>
2022-12-06 13:53:02 +0100

    Day6: Solved!

--- Day6/day6.py
+++ Day6/day6.py
@@ -1,5 +1,8 @@
-RUN_TEST = True
-PART = 1
+from collections import deque
+from functools import partial
+
+RUN_TEST = False
+PART = 2
 
 TEST_INPUT_PATH = 'test_input.txt'
 INPUT_PATH = 'input.txt'
@@ -13,11 +16,41 @@ def main(run_test, part, test_input_path, input_path):
 
 
 def run_part1(input_):
-    pass
+    maxlen = 4
+    # list & map to handle the multiple example inputs
+    proc_buf_packet = partial(process_buffer, maxlen=maxlen)
+    results = list(map(proc_buf_packet, input_))
+    if len(results) == 1:
+        results = results.pop()
+    return results
+
+
+def process_buffer(buf, maxlen):
+    buf_head, buf_tail = split_at(buf, maxlen)
+    q = deque(buf_head, maxlen)
+    for pos, char in enumerate(buf_tail, start=maxlen):
+        if is_unique(q):
+            return pos
+        q.append(char)
+    raise ValueError('ERROR: buffer has no start-of-packet marker')
+
+
+def is_unique(queue):
+    return len(queue) == len(set(queue))
+
+
+def split_at(list_, ind):
+    return list_[:ind], list_[ind:]
 
 
 def run_part2(input_):
-    pass
+    maxlen = 14
+    # list & map to handle the multiple example inputs
+    proc_buf_msg = partial(process_buffer, maxlen=maxlen)
+    results = list(map(proc_buf_msg, input_))
+    if len(results) == 1:
+        results = results.pop()
+    return results
 
 
 def get_input(file_path, line_sep='\n'):

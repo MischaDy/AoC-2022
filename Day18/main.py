0b580857fc4a5633f67c20b7d6981a1ad4394fd2
Mischa <MischaDy@users.noreply.github.com>
2022-12-28 11:01:42 +0100

    Day18: Implemented simpler approach

--- Day18/main.py
+++ Day18/main.py
@@ -1,13 +1,14 @@
-from typing import List
+from itertools import product
+from typing import Set
 
 import numpy as np
 
-RUN_TEST = True
-TEST_NUM = 1
+RUN_TEST = False
+TEST_NUM = 2
 PART = 1
 
 TEST_INPUT_PATH1 = 'test_input1.txt'
-TEST_INPUT_PATH2 = 'test_input1.txt'
+TEST_INPUT_PATH2 = 'test_input2.txt'
 INPUT_PATH = 'input.txt'
 
 
@@ -23,35 +24,43 @@ def main(run_test, test_num, part, test_input_path1, test_input_path2, input_pat
 
 def get_cube_positions(lines):
     for line in lines:
-        yield np.array(line.split(','), dtype=int)
+        yield tuple(map(int, line.split(',')))
 
 
-def get_minmax_coords(cube_positions):
-    num_coords = 3
-    min_coords = np.repeat(np.inf, num_coords)
-    max_coords = np.repeat(-np.inf, num_coords)
-    for cube_pos in cube_positions:
-        min_coords = np.minimum(min_coords, cube_pos)
-        max_coords = np.maximum(max_coords, cube_pos)
-    return min_coords, max_coords
+def get_offsets(num_dims=3):
+    offset_range = [-1, 0, +1]
+    offsets = product(offset_range, repeat=num_dims)
+    # offset changes exactly one coordinate
+    return filter(lambda o: np.abs(o).sum() == 1, offsets)
 
 
-def shift_to_origin(cube_positions: List[np.array], min_coords):
-    rel_cube_positions = cube_positions
-    for cube_pos in cube_positions:
-        cube_pos -= min_coords
-    return rel_cube_positions
+# def get_neighbors(pos, cube_positions: Set[np.ndarray]):
+#     for offset in get_offsets():
+#         neighbor = pos + offset
+#         if neighbor in cube_positions:
+#             yield neighbor
+
+
+def add_elements(iterable1, iterable2, to=tuple):
+    sum_ = []
+    for elem1, elem2 in zip(iterable1, iterable2):
+        sum_.append(elem1 + elem2)
+    return to(sum_)
+
+
+def count_neighbors(pos, cube_positions: Set[tuple]):
+    return sum(1 for offset in get_offsets()
+               if add_elements(pos, offset) in cube_positions)
 
 
 def run_part1(input_):
-    cube_positions = list(get_cube_positions())
-    min_coords, max_coords = get_minmax_coords(cube_positions)
-    rel_cube_positions = shift_to_origin(cube_positions, min_coords)
-
-    grid = np.zeros()
-
-
-
+    cube_positions = set(get_cube_positions(input_))
+    num_faces = 0
+    for cube_pos in cube_positions:
+        num_neighbors = count_neighbors(cube_pos, cube_positions)
+        cube_faces = 6 - num_neighbors
+        num_faces += cube_faces
+    return num_faces
 
 
 def run_part2(input_):

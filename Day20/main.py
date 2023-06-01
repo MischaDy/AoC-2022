507758979a6a417336415b0efd76ef64719898ae
Mischa <MischaDy@users.noreply.github.com>
2023-06-01 14:34:23 +0200

    minor clean-up

--- Day20/main.py
+++ Day20/main.py
@@ -26,7 +26,6 @@ def run_part1_temp(list_):
 
 
 def run_part1(input_):
-    # ls = [[1, 2, -3, 3, -2, 0, 4], [2, 1, -3, 3, -2, 0, 4], [1, -3, 2, 3, -2, 0, 4], [1, 2, 3, -2, -3, 0, 4], [1, 2, -2, -3, 0, 3, 4], [1, 2, -3, 0, 3, 4, -2], [1, 2, -3, 0, 3, 4, -2], [1, 2, -3, 4, 0, 3, -2]]
     nums = list(map(int, input_))
     init_positions = range(len(nums))
     positions = list(init_positions)
